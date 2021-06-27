c69e4ce22f7b606990acc0c472c5f5e31c02b91d
Lucas Martins <lucasmariano.martins@outlook.com>
2021-06-26 21:32:04 -0300

    Create barcode scanner page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/appcolors.dart';
 
 class AppWidget extends StatelessWidget {
+
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
@@ -17,6 +30,8 @@ class AppWidget extends StatelessWidget {
         "/home": (context) => HomePage(),
         "/splash": (context) => SplashPage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
       },
     );
   }
